60b8b9407da2e6633b0e1907325082b86f222c8c
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-08-24 15:18:30 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "ibm" {
-  region     = var.region
+  region = var.region
 }
 
 data "ibm_resource_group" "resource-group" {
@@ -8,10 +8,53 @@ data "ibm_resource_group" "resource-group" {
 
 resource "ibm_function_namespace" "namespace" {
    name                = var.namespace
+   description         = "created by schematics"
    resource_group_id   = data.ibm_resource_group.resource-group.id
 }
 
 resource "ibm_function_package" "package" {
-  name      = var.packagename
+  name      = var.package_name
   namespace = ibm_function_namespace.namespace.name
+}
+
+resource "ibm_function_action" "action" {
+  name      = var.action_name
+  namespace = ibm_function_namespace.namespace.name
+
+  exec {
+    kind  = "openwhisk/dockerskeleton"
+    image = file("setCosContentType.sh")
+  }
+}
+
+resource "ibm_function_trigger" "trigger" {
+  name = var.trigger_name
+  namespace = ibm_function_namespace.namespace.name
+  feed = [
+    {
+      name = "/whisk.system/cos/changes"
+      parameters = <<EOF
+                [
+                        {
+                                "key":"bucket",
+                                "value":"${var.bucket_name}"
+                        },
+                        {
+                                "key":"endpoint",
+                                "value":"s3.private.jp-tok.cloud-object-storage.appdomain.cloud"
+                        },
+                        {
+                                "key":"event_types",
+                                "value":"write"
+                        }
+                ]
+                EOF
+    }
+  ]
+}
+
+resource "ibm_function_rule" "rule" {
+  name         = var.rule_name
+  action_name  = ibm_function_action.action.name
+  trigger_name = ibm_function_trigger.trigger.name
 }
