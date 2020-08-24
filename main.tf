8c8f737fd1f6871091a182ae03f29f5c7d1758d4
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-08-24 14:30:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "ibm" {
   region     = var.region
 }
 
-data "ibm_resource_group" "group" {
-  name = var.resource_group
+data "ibm_resource_group" "resource-group" {
+   name = var.resource_group
+}
+
+resource "ibm_function_namespace" "namespace" {
+   name                = var.namespace
+   resource_group_id   = data.ibm_resource_group.resource-group.id
+}
+
+resource "ibm_function_package" "package" {
+  name      = var.packagename
+  namespace = ibm_function_namespace.namespace.name
 }
