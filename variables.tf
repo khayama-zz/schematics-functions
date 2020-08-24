446a1dbf5de84df88d24c5dc67d2fa90c8943275
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-08-24 15:19:23 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,31 @@
-a
+variable "region" {
+  default = "jp-tok"
+}
+
+variable "resource_group" {
+  default = "khayama-rg"
+}
+
+variable "namespace" {
+  default = "khayama-schematics-fn"
+}
+
+variable "package_name" {
+  default = "khayama-schematics-pkg"
+}
+
+variable "action_name" {
+  default = "khayama-schematics-act"
+}
+
+variable "trigger_name" {
+  default = "khayama-schematics-trg"
+}
+
+variable "bucket_name" {
+  default = "khayama-mime"
+}
+
+variable "rule_name" {
+  default = "khayama-schematics-rule"
+}
